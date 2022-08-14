189315c27f531e36c2f8416e5b843d40800966bd
Berselis Mendoza <berselismendoza@gmail.com>
2022-08-13 21:07:44 -0400

    Deploy
    update code

--- src/components/WikiFinder.jsx
+++ src/components/WikiFinder.jsx
@@ -3,15 +3,14 @@ import WikiInfo from './subcompwikifind/WikiInfo';
 import axios from 'axios';
 import useGetAllLocationAxios from '../assets/hooks/useGetAllLocationAxios';
 import { useState, useEffect } from 'react';
-import { setElemtPagination } from '../assets/js/setPagination';
 
-
-
+import WikiCardNull from './subcompwikifind/WikiCardNull';
+import WikiForm from './subcompwikifind/WikiForm';
 
 const ramdomLocationID = Math.floor(Math.random() * 126);
 
 
-let SUGGEST = []
+let suggestions = []
 
 let singleLocation = {
     name: '-',
@@ -22,6 +21,7 @@ let singleLocation = {
 }
 
 const WikiFinder = () => {
+    suggestions = useGetAllLocationAxios();
     const [queryLocation, setQueryLocation] = useState();
     const [urlLocation, setUrlLocation] = useState(`https://rickandmortyapi.com/api/location/${ramdomLocationID}`);
     const findLocation = (e) => {
@@ -29,95 +29,87 @@ const WikiFinder = () => {
 
         const input = e.target.elements[0];
         if (input.value) {
-            const locationObj = SUGGEST.find(fi => fi.name == input.value)
+            const locationObj = suggestions.find(fi => fi.name == input.value)
             if (locationObj) {
                 setUrlLocation(`https://rickandmortyapi.com/api/location/${locationObj.id}`);
                 input.value = null;
             }
         }
     }
+    
+
+    const [residentCards, setResidentCards] = useState([]);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [cardPerPage, setCardPerPage] = useState(3);
 
     useEffect(() => {
         axios.get(urlLocation)
-            .then(res => setQueryLocation(res.data))
+            .then(res => {
+                setQueryLocation(res.data);
+                setResidentCards(res.data.residents);
+            })
             .catch(error => console.log(error))
     }, [urlLocation]);
-
-
 
     if (queryLocation) {
         singleLocation.name = queryLocation.name || '-';
         singleLocation.type = queryLocation.type || '-';
         singleLocation.dimension = queryLocation.dimension || '-';
         singleLocation.population = queryLocation.residents.length;
+        singleLocation.residents = [];
         if (queryLocation.residents.length > 0) {
             singleLocation.residents = queryLocation.residents;
-            setElemtPagination(15, 2);
-
         }
-
+        
     }
 
 
-    SUGGEST = useGetAllLocationAxios();
+    //Get current residentCard
+    const indexOfLastCard = currentPage * cardPerPage;
+    const indexOfFirstCard = indexOfLastCard - cardPerPage;
+    const currentResidentCards = residentCards.slice(indexOfFirstCard, indexOfLastCard);
 
+
+    
     return (
         <div className='container wiki-finder-main'>
             <div className='row'>
                 <div className='col-md-12 col-sm-12 col-xs-12'>
-
-                    <form onSubmit={findLocation} className='input-group mb-3'>
-                        <input list='locationNames' type='text' className='form-control form-control-lg text-success' placeholder='Write location name' />
-                        <button className='btn btn-success btn-lg btn-finder' type='botton'>Search...</button>
-                        <datalist id='locationNames'>
-                            {
-                                SUGGEST.map(su => {
-                                    return <option key={su.url} value={`${su.name}`} />
-                                })
-                            }
-
-                        </datalist>
-                    </form>
+                   <WikiForm findLocation={findLocation} suggestions={suggestions}/>
                 </div>
 
                 <WikiInfo dataInfo={singleLocation} />
+                <div className='col-md-12 col-sm-12 col-xs-12 wiki-data-pagination'>
+                    <div className='container text-center'>
+
+                        <ul className='list-paginations'>
+
+                            <li className='numb'>1</li>
+                            <li className='numb'>2</li>
+                            <li className='numb active'>3</li>
+                            <li className='numb'>4</li>
+                            <li className='numb'>5</li>
+
+                        </ul>
+
+
+
+                    </div>
+
+                </div>
 
                 <div className='col-md-12 col-sm-12 col-xs-12'>
-
                     <div className='wiki-card-content'>
                         {
-                            singleLocation.residents.map(resident => {
-                                return <WikiCard key={resident} URI={resident} />
-                            })
+                            currentResidentCards.length == 0 ? <WikiCardNull /> : currentResidentCards.map(resident => <WikiCard key={resident} URI={resident} />)
                         }
-
                     </div>
                 </div>
 
 
                 <div className='col-md-12 col-sm-12 col-xs-12 wiki-data-footer'>
-                    <div className='container text-center'>
-                        <div className='wrapper'>
-                            <a className='btn btn-previous'>
-                                <i className="bi bi-arrow-left-circle-fill"></i>
-                            </a>
-                            <ul className='list-pagination'>
-                                <li className="numb"><span>1</span></li>
-                                <li className="numb active "><span>2</span></li>
-                                <li className="numb"><span>3</span></li>
-                                <li className="numb"><span>4</span></li>
-                            </ul>
-                            <a className='btn btn-next'>
-                                <i className="bi bi-arrow-right-circle-fill"></i>
-                            </a>
-
-                        </div>
-
-
-                    </div>
+                    <div className='container text-center'></div>
                 </div>
-
-
             </div>
         </div>
 
---
+++ src/components/subcompwikifind/WikiPaginations.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const WikiPaginations = () => {
+  return (
+    <div>WikiPaginations</div>
+  )
+}
+
+export default WikiPaginations
